e44cd8a1608a9feb5a498529771e3cb604abf6f3
Daniel Sims <danielsims@quickenloans.com>
2014-05-14 14:42:45 -0400

    Moving examples to their own file and bootstrapping

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -2,29 +2,6 @@
 
 use danielgsims\Collection;
 
-class TestClassA{
-  private $v;
-  public function __construct($v){
-    $this->setValue($v);
-  }
-
-  public function getValue(){
-    return $this->v;
-  }
-
-  public function setValue($v){
-    $this->v = $v;
-  }
-}
-
-class TestClassB{
-
-}
-
-class TestClassExtendsA extends TestClassA{
-
-}
-
 class ControllerTest extends PHPUnit_Framework_TestCase {
 
   public function setup(){
---
+++ tests/exampleResources.php
@@ -0,0 +1,26 @@
+<?php
+
+class TestClassA{
+  private $v;
+  public function __construct($v){
+    $this->setValue($v);
+  }
+
+  public function getValue(){
+    return $this->v;
+  }
+
+  public function setValue($v){
+    $this->v = $v;
+  }
+}
+
+class TestClassB{
+
+}
+
+class TestClassExtendsA extends TestClassA{
+
+}
+
+
