888dd60c19028d4c3ee14ba2da2508bd4e240fba
vagrant <vagrant@precise32.(none)>
2014-06-29 03:14:18 +0000

    Wrote and tested tuple

--- src/Tuple.php
+++ src/Tuple.php
@@ -1,12 +1,18 @@
-<?php namespace Collection;
+<?php namespace Collections;
 
-class Tuple implements Countable, IteratorAggregate
+use Collections\Exceptions\ImmutableKeyException;
+use Collections\Exceptions\InvalidArgumentException;
+
+class Tuple implements \Countable, \IteratorAggregate, \ArrayAccess
 {
     private $array;
 
     public function __construct()
     {
         $args = func_get_args();
+        if(empty($args)){
+            throw new InvalidArgumentException("Tuple constructor may not be empty");
+        }
         $this->array = $args;
     }
 
@@ -18,5 +24,30 @@ class Tuple implements Countable, IteratorAggregate
     public function getIterator()
     {
         return new \ArrayIterator($this->array);
+    }
+    
+    public function offsetExists($offest)
+    {
+        return array_key_exists($offest,$this->array);
+    }
+
+    public function offsetGet($offest)
+    {
+        return $this->offsetExists($offest) ? $this->array[$offest] : null; 
+    }
+
+    public function offsetSet($offest, $value)
+    {
+        throw new ImmutableKeyException("A tuple is immutable");
+    }
+
+    public function offsetUnset($offset)
+    {
+        throw new ImmutableKeyException("A tuple is immutable");
+    }
+
+    public function toArray()
+    {
+        return $this->array;
     }
 }
