937e027bd5144e1854fc3ce5c9dd3c9323584838
Thomason <JamesThomason@quickenloans.com>
2014-09-11 12:41:13 -0400

    Add RemoveAll to Collection

--- src/danielgsims/Collection.php
+++ src/danielgsims/Collection.php
@@ -301,7 +301,7 @@ class Collection implements \IteratorAggregate, \Countable
     /**
      * Removes the first item that satisfies the condition callback
      *
-     * @param callback $condition The condition critera to test each item, requires one argument that represents the Collection iterm during iteration.
+     * @param callback $condition The condition critera to test each item, requires one argument that represents the Collection item during iteration.
      * @returns bool Whether the item was found
      */
     public function remove(callable $condition)
@@ -313,6 +313,22 @@ class Collection implements \IteratorAggregate, \Countable
             $this->removeAt($index);
             return true;
         }
+    }
+
+    /**
+     * Removes all items that satisfy the condition callback
+     *
+     * @param callback @condition The condition criteria to test each item, requires on argument that represents the Collection item during interation.
+     * @returns int the number of items found
+     */
+    public function removeAll(callable $condition)
+    {
+        $removed = 0;
+        while($this->remove($condition)) {
+            $removed++;
+        }
+
+        return $removed;
     }
 
     /**
