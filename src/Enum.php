d8ddcee1f03c355bc994a1b898bea20314b89f5f
Brandon Lamb <brlamb@starbucks.com>
2015-01-19 17:25:08 -0800

    fix: Apply PSR formatting, add missing docblocks

--- src/Enum.php
+++ src/Enum.php
@@ -1,60 +1,95 @@
-<?php namespace Collections;
+<?php
+
+namespace Collections;
 
 use Collections\Exceptions\ImmutableKeyException;
+use ArrayAccess;
+use ArrayIterator;
+use Countable;
+use IteratorAggregate;
 
-class Enum implements \ArrayAccess, \IteratorAggregate, \Countable
+class Enum implements ArrayAccess, Countable, IteratorAggregate
 {
-    protected $array;
+    protected $storage;
 
-    public function __construct(array $array = array())
+    /**
+     * Constructor
+     *
+     * @param array $storage
+     */
+    public function __construct(array $storage = array())
     {
-        $this->array = $array;
+        $this->storage = $storage;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function offsetExists($offest)
     {
-        return array_key_exists($offest,$this->array);
+        return array_key_exists($offest, $this->storage);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function offsetGet($offest)
     {
-        return $this->offsetExists($offest) ? $this->array[$offest] : null; 
+        return $this->offsetExists($offest) ? $this->storage[$offest] : null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function offsetSet($offest, $value)
     {
         throw new ImmutableKeyException("You cannot add keys to an Enum after construction.");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function offsetUnset($offset)
     {
         throw new ImmutableKeyException("You cannot remove a key from an Enum.");
     }
 
-    public function toArray()
-    {
-        return $this->array;
-    }
-
+    /**
+     * {@inheritDoc}
+     */
     public function keyExists($key)
     {
-        return array_key_exists($key,$this->array);
-    }   
-
-    public function getIterator()
-    {
-        $items = $this->array;
-
-        return new \ArrayIterator($items);
+        return array_key_exists($key, $this->storage);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    public function getIterator()
+    {
+        return new ArrayIterator($this->storage);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     public function count()
     {
-        return count($this->array);
+        return count($this->storage);
+    }
+
+    /**
+     * Return internal storage array
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->storage;
     }
 
     public function valueExists($value)
     {
-        return in_array($value, $this->array);
+        return in_array($value, $this->storage);
     }
 }
--- src/Tuple.php
+++ src/Tuple.php
@@ -1,53 +1,102 @@
-<?php namespace Collections;
+<?php
+
+namespace Collections;
 
 use Collections\Exceptions\ImmutableKeyException;
 use Collections\Exceptions\InvalidArgumentException;
+use ArrayAccess;
+use ArrayIterator;
+use Countable;
+use IteratorAggregate;
 
-class Tuple implements \Countable, \IteratorAggregate, \ArrayAccess
+class Tuple implements ArrayAccess, Countable, IteratorAggregate
 {
-    private $array;
+    protected $storage;
 
+    /**
+     * Constructor
+     *
+     * @param array $params, PHP 5.6 variadic function
+     */
+    /*
+    public function __construct(...$params)
+    {
+        if (empty($params)) {
+            throw new InvalidArgumentException("Tuple constructor may not be empty");
+        }
+        $this->storage = $params;
+    }
+    */
+
+    /**
+     * Constructor
+     *
+     * Parses function arguments
+     */
     public function __construct()
     {
         $args = func_get_args();
-        if(empty($args)){
+        if (empty($args)) {
             throw new InvalidArgumentException("Tuple constructor may not be empty");
         }
-        $this->array = $args;
+        $this->storage = $args;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function count()
     {
-        return count($this->array);
+        return count($this->storage);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getIterator()
     {
-        return new \ArrayIterator($this->array);
+        return new ArrayIterator($this->storage);
     }
-    
+
+    /**
+     * {@inheritDoc}
+     */
     public function offsetExists($offest)
     {
-        return array_key_exists($offest,$this->array);
+        return array_key_exists($offest, $this->storage);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function offsetGet($offest)
     {
-        return $this->offsetExists($offest) ? $this->array[$offest] : null; 
+        return $this->offsetExists($offest) ? $this->storage[$offest] : null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function offsetSet($offest, $value)
     {
-        throw new ImmutableKeyException("A tuple is immutable");
+        throw new ImmutableKeyException('A tuple is immutable');
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function offsetUnset($offset)
     {
-        throw new ImmutableKeyException("A tuple is immutable");
+        throw new ImmutableKeyException('A tuple is immutable');
     }
 
+    /**
+     * Return internal storage array
+     *
+     * @return array
+     */
     public function toArray()
     {
-        return $this->array;
+        return $this->storage;
     }
 }
