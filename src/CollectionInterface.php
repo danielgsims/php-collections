0a9e96a3c0b05c0ae5cc8060670bcca4d8142ee2
Jakub Giminski <jakubgiminski@gmail.com>
2017-02-28 10:15:23 +0000

    Adds first() and last() methods to ColletionInterface

--- src/CollectionInterface.php
+++ src/CollectionInterface.php
@@ -313,4 +313,20 @@ interface CollectionInterface extends \IteratorAggregate, \Countable
      * @throws InvalidArgumentException
      */
     public function merge($items);
+
+    /**
+     * Get first item of the collection
+     *
+     * @return mixed
+     * @throws \RuntimeException
+     */
+    public function first();
+
+    /**
+     * Get last item of the collection
+     *
+     * @return mixed
+     * @throws \RuntimeException
+     */
+    public function last();
 }
