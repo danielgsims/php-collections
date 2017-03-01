dfbe8f9c7c4fc7a350341589c6f5d47eeb933281
Dawid Kraczkowski <dawid.kraczkowski@gmail.com>
2017-03-01 03:11:50 +0100

    Invalid state bug + refactor (#42)

--- src/TypeValidator.php
+++ src/TypeValidator.php
@@ -13,36 +13,36 @@ trait TypeValidator
         }
 
         if ($scalarType = $this->determineScalar($type)) {
-            if ($keyType && (in_array($scalarType, ["double", "boolean"]))) {
-                throw new InvalidArgumentException("This type is not supported as a key.");
+            if ($keyType && (in_array($scalarType, ['double', 'boolean']))) {
+                throw new InvalidArgumentException('This type is not supported as a key.');
             }
 
             return $scalarType;
         }
 
-        throw new InvalidArgumentException("This type does not exist.");
+        throw new InvalidArgumentException('This type does not exist.');
     }
 
     private function nonScalarTypeExists($type)
     {
         return class_exists($type)
                 || interface_exists($type)
-                || in_array($type, ["array", "object", "callable"]);
+                || in_array($type, ['array', 'object', 'callable']);
     }
 
     private function determineScalar($type)
     {
         $synonyms = [
-            "int" => "integer",
-            "float" => "double",
-            "bool" => "boolean"
+            'int' => 'integer',
+            'float' => 'double',
+            'bool' => 'boolean'
         ];
 
         if (array_key_exists($type, $synonyms)) {
             $type = $synonyms[$type];
         }
 
-        $types = [ "string", "integer", "double", "boolean" ];
+        $types = [ 'string', 'integer', 'double', 'boolean' ];
 
         return in_array($type, $types) ? $type : null;
     }
@@ -65,22 +65,22 @@ trait TypeValidator
         $type = gettype($item);
 
         $shouldBeCallable = $target === 'callable';
-        $isObject = $type === "object";
-        $looseObjectCheck = $target === "object";
+        $isObject = $type === 'object';
+        $looseObjectCheck = $target === 'object';
 
         //callable must be callable
         if ($shouldBeCallable && !is_callable($item)) {
-            throw new InvalidArgumentException("Item must be callable");
+            throw new InvalidArgumentException('Item must be callable');
         }
 
         //target isn't callable, object must be an instance of target
-        if (!$shouldBeCallable && $isObject && !is_a($item, $target) && !$looseObjectCheck) {
-            throw new InvalidArgumentException("Item is not type or subtype of " . $target);
+        if (!$looseObjectCheck && !$shouldBeCallable && $isObject && !is_a($item, $target)) {
+            throw new InvalidArgumentException("Item is not type or subtype of $target");
         }
 
         //a non callable, non object type should match the target string
-        if (!$shouldBeCallable && !$isObject && $type != $target) {
-            throw new InvalidArgumentException("Item is not of type: " . $target);
+        if (!$shouldBeCallable && !$isObject && $type !== $target) {
+            throw new InvalidArgumentException("Item is not of type: $target");
         }
     }
 }
